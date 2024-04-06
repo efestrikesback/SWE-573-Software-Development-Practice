0109ca6bc385ad87d038f36e5dcbf6be7a0b12e3
efestrikesback <96078086+efestrikesback@users.noreply.github.com>
2024-04-06 17:24:59 +0300

    #6 completed Java SDK & Next.Js setup

---
+++ devcom-web/src/main/java/com/devcom/webapp/DevcomWebApplication.java
@@ -0,0 +1,13 @@
+package com.devcom.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcomWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcomWebApplication.class, args);
+	}
+
+}
