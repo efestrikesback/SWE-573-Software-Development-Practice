cd53ab8bd624317a159216218172fe3fdca234d4
efestrikesback <96078086+efestrikesback@users.noreply.github.com>
2024-04-07 00:13:13 +0300

    #15 fixed JPA bug

--- devcom-web/src/main/java/com/devcom/webapp/DevcomWebApplication.java
+++ devcom-web/src/main/java/com/devcom/webapp/DevcomWebApplication.java
@@ -2,8 +2,16 @@ package com.devcom.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EnableJpaRepositories("com.devcom.webapp.models")
+@ComponentScan(basePackages = { "com.devcom.webapp.models" })
+@EntityScan(basePackages = {"com.devcom.webapp.models"})
 @SpringBootApplication
+
 public class DevcomWebApplication {
 
 	public static void main(String[] args) {
--- devcom-web/src/main/java/com/devcom/webapp/repositories/UserRepository.java
+++ devcom-web/src/main/java/com/devcom/webapp/repositories/UserRepository.java
@@ -4,5 +4,6 @@ package com.devcom.webapp.repositories;
 import com.devcom.webapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
